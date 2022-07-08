c2b50e713cc84bb9e8897dcd0f64a879711a4b60
EslamBanna <solombana2000@gmail.com>
2022-07-08 18:25:33 +0200

    finish style test

--- app/Http/Controllers/ExamsResultController.php
+++ app/Http/Controllers/ExamsResultController.php
@@ -59,12 +59,14 @@ class ExamsResultController extends Controller
                 $user_exam = ExamsResult::find($user_exam[0]->id);
                 $user_exam->update([
                     "first_exam" => json_encode($exam),
+                    'note' => $request->note,
                 ]);
             } else {
                 // it means that it's the first test for the user
                 $user_exam = ExamsResult::create([
                     "first_exam" => json_encode($exam),
-                    "user_id" => Auth()->user()->id
+                    "user_id" => Auth()->user()->id,
+                    'note' => $request->note,
                 ]);
             }
             return $this->returnData('data', $face_shape);
@@ -143,12 +145,12 @@ class ExamsResultController extends Controller
             //determine body type
             if (count($max_choices) == 1) {
                 if ($max_choices[0] == 'shoulder') {
-                    if ($request->Shoulder - $request->Buttock > 5) {
+                    if ($request->shoulder - $request->buttock > 5) {
                         $result = 'Inverted_Triangle';
                     } else {
-                        if ($request->Themiddle == "unspecific") {
+                        if ($request->the_middle == "unspecific") {
                             $result = 'Rectangle';
-                        } elseif ($request->Themiddle == "specific") {
+                        } elseif ($request->the_middle == "specific") {
                             $result = 'clock';
                         } else {
                             $result = 'Inverted_Triangle';
@@ -197,15 +199,157 @@ class ExamsResultController extends Controller
                 $user_exam = ExamsResult::find($user_exam[0]->id);
                 $user_exam->update([
                     "second_exam" => json_encode($exam),
+                    'note' => $request->note,
                 ]);
             } else {
                 // it means that it's the first test for the user
                 $user_exam = ExamsResult::create([
                     "second_exam" => json_encode($exam),
-                    "user_id" => Auth()->user()->id
+                    "user_id" => Auth()->user()->id,
+                    'note' => $request->note,
                 ]);
             }
 
+            return $this->returnData('data', $result);
+        } catch (\Exception $e) {
+            return $this->returnError(202, $e->getMessage());
+        }
+    }
+
+    public function styleTest(Request $request)
+    {
+        try {
+            //validation
+            $validate = Validator($request->all(), [
+                "color" => "required|in:1,2,3,4,5,6,7",
+                "makeup" => "required|in:1,2,3,4,5,6,7",
+                "hair_style" => "required|in:1,2,3,4,5,6,7",
+                "dress" => "required|in:1,2,3,4,5,6,7",
+                "blouse" => "required|in:1,2,3,4,5,6,7",
+                "pants" => "required|in:1,2,3,4,5,6,7",
+                "skirt" => "required|in:1,2,3,4,5,6,7",
+                "shoes" => "required|in:1,2,3,4,5,6,7",
+                "bag" => "required|in:1,2,3,4,5,6,7",
+                "jacket" => "required|in:1,2,3,4,5,6,7",
+                "accessory" => "required|in:1,2,3,4,5,6,7",
+                "appearance" => "required|in:1,2,3,4,5,6,7"
+            ]);
+            if ($validate->fails()) {
+                return $this->returnError(202, $validate->errors()->first());
+            }
+            $romantic = 0;
+            $classic = 0;
+            $Casual = 0;
+            $dramatic = 0;
+            $Bohemian = 0;
+            $Ancient = 0;
+            $Gamine = 0;
+            $user_choises = [];
+            $style = [];
+            $result = [];
+            $style_comp = "";
+            //
+            $user_choises[] = $request->color;
+            $user_choises[] = $request->makeup;
+            $user_choises[] = $request->hair_style;
+            $user_choises[] = $request->dress;
+            $user_choises[] = $request->blouse;
+            $user_choises[] = $request->pants;
+            $user_choises[] = $request->skirt;
+            $user_choises[] = $request->shoes;
+            $user_choises[] = $request->bag;
+            $user_choises[] = $request->jacket;
+            $user_choises[] = $request->accessory;
+            $user_choises[] = $request->appearance;
+            // dd($user_choises);
+
+            // every input determine a choice and it's name has number which tell you which style is that
+            foreach ($user_choises as $choise) {
+                if (strpos($choise, '1') !== false) {
+                    $romantic = $romantic + 1;
+                }
+                if (strpos($choise, '2') !== false) {
+                    $classic = $classic + 1;
+                }
+                if (strpos($choise, '3') !== false) {
+                    $Casual = $Casual + 1;
+                }
+                if (strpos($choise, '4') !== false) {
+                    $dramatic = $dramatic + 1;
+                }
+                if (strpos($choise, '5') !== false) {
+                    $Bohemian = $Bohemian + 1;
+                }
+                if (strpos($choise, '6') !== false) {
+                    $Ancient = $Ancient + 1;
+                }
+                if (strpos($choise, '7') !== false) {
+                    $Gamine = $Gamine + 1;
+                }
+            }
+
+            //to get the maximum choices number
+            $styles = array($romantic, $classic, $Casual, $dramatic, $Bohemian, $Ancient, $Gamine);
+            $value = max($styles);
+
+            if ($romantic == $value) {
+                $style[] = "Romantic";
+            }
+            if ($classic == $value) {
+                $style[] = "Classic";
+            }
+            if ($Casual == $value) {
+                $style[] = "Casual";
+            }
+            if ($dramatic == $value) {
+                $style[] = "Dramatic";
+            }
+            if ($Bohemian == $value) {
+                $style[] = "Bohemian";
+            }
+            if ($Ancient == $value) {
+                $style[] = "Ancient";
+            }
+            if ($Gamine == $value) {
+                $style[] = "Gamine";
+            }
+
+            //check style type , single or composite
+            if (count($style) == 1) {
+                $style_type = "single";
+                $result = $style[0];
+            } else {
+                $style_type = "composite";
+                $result[] = $style[0]; // thats the first style
+                $result[] = $style[1]; // thats the second style
+            }
+
+            //because first_exam column with json datatype
+            $exam = array(
+                'style_type' => $style_type,
+                "style" => $result, // if single then result will be sting and if composite will be array of two styles
+                "user choices" => $request->all()
+            );
+
+
+            // //find if user exams results was exists or not
+            $user_exam = ExamsResult::where('user_id', '=', Auth()->user()->id)->get();
+
+            // if count of user greater than 0 , it means that he has rows befor show so he tested befor , then we just should edit exam result
+            if (count($user_exam) != 0) {
+                $user_exam = ExamsResult::find($user_exam[0]->id);
+                $user_exam->update([
+                    "third_exam" => json_encode($exam),
+                    'note' => $request->note,
+                ]);
+            } else {
+                // it means that it's the first test for the user
+                $user_exam = ExamsResult::create([
+                    "third_exam" => json_encode($exam),
+                    "user_id" => Auth()->user()->id,
+                    'note' => $request->note,
+                ]);
+            }
             return $this->returnData('data', $result);
         } catch (\Exception $e) {
             return $this->returnError(202, $e->getMessage());
